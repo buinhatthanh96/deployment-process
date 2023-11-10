e766689408cd62bba5966e58b58a967031e146eb
Thanh Bui <thanhbn3@fsoft.com>
2023-11-10 17:28:16 +0700

    fix apihost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-zzq2trcz.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-up8gzxup.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
